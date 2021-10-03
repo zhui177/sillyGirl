01ec4db1ecb43aebfda9d9252b5983ae5acdbe5d
zhui177 <56864906+zhui177@users.noreply.github.com>
2021-10-03 10:16:59 +0800

    Update dev.go

--- dev.go
+++ dev.go
@@ -4,7 +4,7 @@ import (
          //青龙2.9机器人
 	 _ "github.com/cdle/sillyGirl/develop/qinglong"
 	 //京东账号
-	 //_ "github.com/cdle/sillyGirl/develop/jd_cookie"
+	 _ "github.com/cdle/sillyGirl/develop/jd_cookie"
 	 //腾讯QQ
 	 _ "github.com/cdle/sillyGirl/im/qq"
 	 //telegram
